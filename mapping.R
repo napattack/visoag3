0f6729ad7f7c4e4eb19edca4172a867921f5fd6d
napattack <64198325+napattack@users.noreply.github.com>
2021-09-14 18:28:44 +0200

    upload interatcive table

--- mapping.R
+++ mapping.R
@@ -2,8 +2,9 @@ library(shiny)
 library(leaflet)
 library(tidyverse)
 library(htmlwidgets)
+library(DT)
 
-
+#clean datatable and saved, you dont have to run this
 #Score<-read.csv("data/cleanScore.csv")
 #data<-read.csv("data/geo_data_woduplicateitem.csv")
 #join<-left_join(Score,data)
@@ -45,55 +46,72 @@ ui <- fluidPage(
                                      ,"Bayern","Niedersachsen"  , "Brandenburg"           
                                      , "Mecklenburg-Vorpommern" ,"Schleswig-Holstein"  ,"Thüringen"             
                                      ,"Sachsen" ,"Rheinland-Pfalz" , "Saarland"              
-                                     ,"Bremen"))
+                                     ,"Bremen")),
+  downloadButton("doenloadData","Download")
   
   ),
   mainPanel(
   width = 10,
   leafletOutput("mymap"),
-  tableOutput("mydata"),
+  tableOutput("mytable"),
+#  dataTableOutput("mytable"),
   p(),
   hr(),
   br(),
   tags$a("Die Datensammlung des BAOA entsteht im Rahmen des Projekts open-access.network durch den Projektpartner Open-Access-Büro Berlin an der Universitätsbibliothek der Freien Universität Berlin. Allgemeine Informationen zum Projekt und dem Konzept des Bundesländer-Atlas können in der aktuellen Version 3.0 des Konzeptpapiers abgerufen werden (DOI: https://doi.org/10.5281/zenodo.4644125). An der Erstellung der Daten waren beteiligt: Maxi Kindling, Sophie Kobialka, Maike Neufend, Agnieszka Wenninger (Stand: 4.9.2021)")
-    )
+    
+  )
   )
 )
-pal<-colorBin(palette = "OrRd",9,domain = map.data$OpenScore_institution)
+pal<-colorBin(palette = "OrRd",7,domain = map.data$OpenScore_institution)
 server <- function( input, output) {
+   dataset<- reactive({
+  if(is.null(input$insType)&is.null(input$bund)){
+    dataset<-map.data
+  }else if(is.null(input$insType)&!is.null(input$bund)){
+    dataset<-map.data%>%
+      filter(X%in%input$bund)
+  }else if(is.null(input$bund)&!is.null(input$insType)){
+    dataset<-map.data%>%
+      filter(institutionType%in%c(input$insType))
+  }
+  else{
+    dataset<-map.data%>%
+      filter(institutionType%in%c(input$insType))%>%
+      filter(X%in%input$bund)
+  }
+   })
   #make interactive app 
-  output$mymap <- renderLeaflet({
-    if(is.null(input$insType)&is.null(input$bund)){
-      dataset<-map.data
-    }else if(is.null(input$insType)&!is.null(input$bund)){
-      dataset<-map.data%>%
-        filter(X%in%input$bund)
-    }else if(is.null(input$bund)&!is.null(input$insType)){
-      dataset<-map.data%>%
-        filter(institutionType%in%c(input$insType))
-      }
-    else{
-      dataset<-map.data%>%
-        filter(institutionType%in%c(input$insType))%>%
-        filter(X%in%input$bund)
-    }
-     
-    leaflet()%>%
+  output$mymap <- renderLeaflet(
+    {
+     leaflet()%>%
       addProviderTiles(provider="CartoDB.Positron")%>%
      # addTiles(tags$a(paste0("Map of OA level by",as.character(input$insType))),)%>%
       addCircleMarkers(
-        data= dataset,
-        lat =dataset$lat,lng = dataset$lon,radius =~15*OpenScore_institution*0.01,
-        color = ~pal(OpenScore_institution),fillColor = ~pal(OpenScore_institution),opacity = 1,fillOpacity = .8,
-        label = paste0(dataset$Name.der.Institution,": ",as.character(dataset$OpenScore_institution),"% ", as.character(dataset$Ort),": ",as.character(dataset$bundesländer_score),"% Germany:",as.character(dataset$country_score),"%"),
-        stroke = FALSE
-        ,popup=dataset$additional.infos)%>%
         
-      addLegend("bottomright",
+        data= dataset(),
+        lat =dataset()$lat,lng = dataset()$lon,radius =~15*dataset()$OpenScore_institution*0.01,
+        color = ~pal(OpenScore_institution),fillColor = ~pal(OpenScore_institution),opacity = 1,fillOpacity = .8,
+        label = paste0(dataset()$Name.der.Institution,": ",as.character(dataset()$OpenScore_institution),"% ", as.character(dataset()$Ort),": ",as.character(dataset()$bundesländer_score),"% Germany:",as.character(dataset()$country_score),"%"),
+        stroke = FALSE
+        ,popup=dataset()$additional.infos)%>%
+        addLegend("bottomright",
                 pal=pal,
                 values=paste0(as.character(~OpenScore_institution),"%"),
                 opacity=0.7 ,
                 title = "Erfüllte OA-Indikatoren Rate") } )
+    
+     output$mytable<-renderTable({
+      dataset()
+    })
+    output$downloadData <- downloadHandler(
+      filename = function() {
+        paste("BAOA",date(), ".csv", sep = "")
+      },
+      content = function() {
+        write.csv(dataset(), row.names = FALSE)
+      }
+    )
       
 }
 
